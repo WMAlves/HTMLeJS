76c9222aa34cb4b3928f5e38ae049a833adf1bde
WMAlves <williamma03@gmail.com>
2022-11-06 23:55:38 -0300

    alterações dos arquivos html e main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-function ola(){
+window.onload = () =>{
 
-    //janelas modal
-    let nome= prompt('Qual o seu nome?', 'José Silva');
-    alert(`Olá ${nome}`);
-    //janela modal de confirmação
-    let confirma= confirm(`Confirma a operação ${nome} ?`);
-    if(confirma)
-    alert('Operação Confirmada');
-    else alert('Operação Cancelada');
+    bt.onclick = function ola(){
+
+        //janelas modal
+        let nome= prompt('Qual o seu nome?', 'José Silva');
+        alert(`Olá ${nome}`);
+        //janela modal de confirmação
+        let confirma= confirm(`Confirma a operação ${nome} ?`);
+        if(confirma)
+        alert('Operação Confirmada');
+        else alert('Operação Cancelada');
+    }
 }
