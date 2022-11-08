323e25632776b6b518962ccd4b5d18af07c7d296
WMAlves <williamma03@gmail.com>
2022-11-08 13:04:52 -0300

    3º Alteração HTML e JAvascrip
    Criados alteração do HTML utilizando o Javascript e adicionado um evento em um botão.

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-window.onload = () =>{
+window.onload = () =>{     //função necessária para carregar o html todo antes de carregar o javascript
 
-    bt.onclick = function ola(){
-
-        //janelas modal
-        let nome= prompt('Qual o seu nome?', 'José Silva');
-        alert(`Olá ${nome}`);
-        //janela modal de confirmação
-        let confirma= confirm(`Confirma a operação ${nome} ?`);
-        if(confirma)
-        alert('Operação Confirmada');
-        else alert('Operação Cancelada');
+    let intro= document.getElementById('introducao');
+    intro.innerHTML
+    intro.style.textAlign='center';
+    intro.style.fontWeight = 'bold';
+    intro.style.padding='10px 5px';
+    intro.style.border='thin sold black';
+    intro.style.background='#ddf';    
+    
+    bt.onclick = ()=>{
+        let lista = document.getElementById('lista');
+        lista.innerHTML += '<li>Item</item>';
     }
-}
+ 
+    }
+
+ 
+
+
+
+// parei de ver a video aula da puc no momento 15:58, continue daqui. 
