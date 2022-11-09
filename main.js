c2dd719adc091f9b0ab775f1519c56862928927f
WMAlves <williamma03@gmail.com>
2022-11-09 14:02:41 -0300

    inicio de tratamento de fomrulários, parei na video aula no tempo: 13:40.

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-window.onload = () =>{     //função necessária para carregar o html todo antes de carregar o javascript
-
-    let intro= document.getElementById('introducao');
-    intro.innerHTML
-    intro.style.textAlign='center';
-    intro.style.fontWeight = 'bold';
-    intro.style.padding='10px 5px';
-    intro.style.border='thin sold black';
-    intro.style.background='#ddf';    
-    
-    bt.onclick = ()=>{
-        let listaAdd = document.getElementById('listaAdd');
-        listaAdd.innerHTML += '<li>Item</item>';
+window.onload = () => {
+    let nome = document.getElementById('nome');
+    //onchange verifica se há alguma coisa alterada.
+    nome.onchange=()=>{
+        console.log('change',nome.value);
+    };
+    nome.oninput=()=>{ //verificar alteração no campo
+        console.log('input', nome.value.length);    
+    };
+    //nome.oninput=()=>{
+        //let caracterInvalido ='!@$%¨&*()-=<>,.;[]{}';
+        //let ultimoCaracter=nome.value.charArt(nome.value.length-1);
+        //if(caracterInvalido.indexOf(ultimoCaracter)>=0)
+        //nome.value=nome.value.substr(0,nome.value.length-1);
+    //}
+    let senha = document.getElementById('senha');
+    senha.onchange=()=>{
+        console.log(senha.value);
     }
-        let li = document.querySelectorAll('li');        
-        console.log(lista);
-    }
 
- 
-
-
-
-// parei de ver a video aula da puc no momento 15:58, continue daqui. 
+};
